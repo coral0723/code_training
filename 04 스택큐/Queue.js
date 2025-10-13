9127f986b781141b764c0e33e741e09f5aaa8d02
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-10-13 13:56:25 +0900

    04 스택 큐 - Queue, Deque 추가

---
+++ 04 스택큐/Queue.js
@@ -0,0 +1,22 @@
+class Queue {
+  constructor() {
+    this.front = 0; // 선두 항목이 들어있는 칸의 index
+    this.rear = 0; // 마지막 항목이 들어있는 칸 다음 빈 칸 index
+    this.storage = new Map();
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  add(value) {
+    this.storage.set(this.rear++, value);
+  }
+
+  remove() {
+    if (this.size() === 0) return;
+    const value = this.storage.get(this.front);
+    this.storage.delete(this.front++);
+    return value;
+  }
+}
