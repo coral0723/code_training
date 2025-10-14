cdb7903a634cb4ccd6c357bda081fa1c64c01a5c
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-10-14 11:05:05 +0900

    힙 - MaxHeap 추가

---
+++ 06 힙/MaxHeap.js
@@ -0,0 +1,48 @@
+class MaxHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp(this.heap.length - 1);
+  }
+
+  heapifyUp(index) {
+    while(index > 0) {
+      const parentIndex = (index - 1) >> 1; // 부모 노드 인덱스: (k - 1) / 2
+      if (this.heap[parentIndex] >= this.heap[index]) break;
+      [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+      index = parentIndex;
+    }
+  }
+
+  remove() {
+    const min = this.heap[0];
+    const end = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = end;
+      this.heapifyDown(0);
+    }
+    return min;
+  }
+
+  heapifyDown(index) {
+    while (index < this.heap.length) {
+      const left = (index << 1) + 1; // 왼쪽 자식 노드: 2k + 1
+      const right = (index << 1) + 2; // 오른쪽 자식 노드: 2k + 2
+      let smallest = index;
+      if (left < this.heap.length && this.heap[left] > this.heap[smallest]) // left 인덱스가 heap 범위 내이고, smallest보다 크다면
+        smallest = left;
+      if (right < this.heap.length && this.heap[right] > this.heap[smallest]) // right 인덱스가 heap 범위 내이고, smallest보다 크다면
+        smallest = right;
+      if (smallest === index) break; // 현재 index가 smallest라면
+      [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]]; // smallest와 index의 값 교환
+      index = smallest;
+    }
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+}
